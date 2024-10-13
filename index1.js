3c9ae336acc217e0278cac45c229d342324575e4
Кірюша <mkiril087@gmail.com>
2024-10-13 10:40:49 +0300

    пр

---
+++ index1.js
@@ -0,0 +1,57 @@
+// task 1
+// let nums = [1, 9, 3];
+// nums[1] = 2;
+
+// console.log(nums);
+
+
+// task2
+
+// let strings = [`red`, `color`, `opacity`]
+// strings.push(`something`)
+// console.log(strings);
+
+
+// task3
+
+// let nums = [1, 9, 3];
+
+// for (let i = 0; i < 1; i += 1) {
+//     console.log(nums[i] + nums[i + 1] + nums[i + 2]);
+    
+// }
+
+// task4
+
+// let nums = [1, 2, 4, 3, 5];
+
+// for (let i = 0; i <= 5; i += 1) {
+//     console.log(nums[i]);
+    
+// }
+
+// task5
+
+// let strings = [`join`, `republic`, `Ukraine`, `sour-apple`, `tree`];
+
+// for (let i = 0; i <= 5; i += 1) {
+//     if (strings[i].length > 5) {
+//         console.log(strings[i]);
+        
+//     }
+// }
+
+// task6
+
+// let nums = [1, 3, 2, 10, 34, 17, 39, 82, 51, 52];
+
+// for (let i = 0; i < 11; i += 1) {
+//     if (nums[i] >= nums[0] && nums[i] >= nums[1] && nums[i] >= nums[2] && nums[i] >= nums[3] && nums[i] >= nums[4] && nums[i] >= nums[5] && nums[i] >= nums[6] && nums[i] >= nums[7] && nums[i] >= nums[8] && nums[i] >= nums[9]) {
+//         console.log(nums[i]);
+        
+//     }
+// } 
+
+// task7
+
+// не зрозумів завдання
